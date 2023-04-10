44ec8c95bcf3d812e41847c84001e39b27c96651
Said Albujar Torres <saidalbujar23@gmail.com>
2023-04-10 18:58:43 -0500

    Correcion en el script arma

--- Inventario Armas/Inventario Armas/Arma.cs
+++ Inventario Armas/Inventario Armas/Arma.cs
@@ -6,11 +6,21 @@ namespace Inventario_Armas
 {
     class Arma
     {
-        
+        protected string nombre;
+        protected float dmg;
+        protected float aVelocidad;
+        protected float precio;
 
+        public Arma(string nombre, float dmg, float aVelocidad, float precio) : base(precio)
+        {
+            this.nombre = nombre;
+            this.dmg = dmg;
+            this.precio = precio;
+            
+        }
         public virtual float DPS()
         {
-            return 0;
+            return dmg * aVelocidad;
         }
     }
 }
